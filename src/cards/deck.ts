bf0323e800843eb8385048c6c4b147bcd8ef3b06
Aiden Cline <aidenpcline@gmail.com>
2023-11-07 11:25:31 -0600

    some more updates

--- src/cards/deck.ts
+++ src/cards/deck.ts
@@ -1,14 +1,27 @@
 import * as cards from './cards'
 
-export interface Deck {
-    url: string,
-    cards: cards.Card[],
+export class Deck {
+    url: string;
+    cards: cards.Card[];
+    valid: boolean;
+
+    constructor() {
+        this.cards = [];
+        this.url = "";
+        this.valid = false;
+    }
+
+    addCard(card: cards.Card) {
+        this.cards.push(card);
+        if (this.cards.length == 8) {
+            this.valid = true;
+            this.url = getUrl(this.cards);
+        }
+    }
 }
 
-// Perhaps we should create a class here...
-
 function getUrl(cardArr: cards.Card[]) {
-    let url = ' https://link.clashroyale.com/deck/en?deck='
+    let url = 'https://link.clashroyale.com/deck/en?deck='
     cardArr.forEach(card => {
         url += `${card.id};`
     })
