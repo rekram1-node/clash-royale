b6fa2ee7f90b9d233a607cae2a4ae75034cbdd3d
Aiden Cline <aidenpcline@gmail.com>
2023-11-08 11:55:18 -0600

    working image rendering

--- output.ts
+++
@@ -1,6 +0,0 @@
-import { writeFile, writeFileSync } from 'fs';
-import { createMegaDraft } from './src/models/megaDraft';
-
-console.log(createMegaDraft().cards.length)
-
-writeFileSync("out.json", JSON.stringify(createMegaDraft().cards))
